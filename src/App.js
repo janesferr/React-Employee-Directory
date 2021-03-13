70e087bdc7a0e89c3656b491ec505f51b2eb502e
jansferr <sferrazzajane@gmail.com>
2021-03-13 15:36:03 -0500

    draft

--- src/App.js
+++ src/App.js
@@ -1,80 +1,70 @@
 import React, { Component } from "react";
 // import axios from 'axios';
 // import ReactDOM from 'react-dom'
-import FriendCard from './components/FriendCard'
+// import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import FriendCard from './components/FriendCard';
+import Table from 'react-bootstrap/Table'
 
 
 class App extends Component {
-    constructor(){
-        super();
-        this.state = {
-          users: []
-        };
-      }
-    
-      componentWillMount() {
-        fetch('https://randomuser.me/api/?results=50')
-        .then(response => {
-          if(response.ok) return response.json();
-          throw new Error('Request failed.');
-        })
-        .then(data => {
-          this.setState({users: data.results});
-        })
-        .catch(error => {
-          console.log(error);
-        });
-      }
-    
-      render() {
-        const list = this.state.users.map( (u, i) => {
-            return <User key={u.login.md5} name={`${u.name.first} ${u.name.last}`} email={u.email} phone={u.phone} />;
-        });
-        return (
-          <div>
-            <h1>Employee Directory</h1>
+  constructor() {
+    super();
+    this.state = {
+      users: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://randomuser.me/api/?results=50')
+      .then(response => {
+        if (response.ok) return response.json();
+        throw new Error('Request failed.');
+      })
+      .then(data => {
+        this.setState({ users: data.results });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  render() {
+
+    const list = this.state.users.map((u, i) => (
+      <FriendCard
+        key={u.login.md5}
+        id={i+1}
+        name={`${u.name.first} ${u.name.last}`}
+        email={u.email}
+        phone={u.phone}
+        image={u.picture.medium}
+      />
+    ));
+    return (
+      <div>
+        <Title> Employee Directory</Title>
+        <Table striped bordered hover variant="light" >
+          <thead>
+            <tr>
+              <th>#</th>
+              <th> Name </th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Picture</th>
+            </tr>
+
+          </thead>
+          <tbody>
             {list}
-          </div>
-        );
-      }
-    }
-    
-    class User extends React.Component {
-      render() {
-        return (
-          <div>
-         <div className="card">
-    <div className="img-container">
-            <FriendCard 
-            name = {this.props.name}
-            // image= {this.props.picture.large}
-            phone = {this.props.phone}
-            email={this.props.email}
-            />
-            
-      {/* <img alt={this.props.name} src={this.props.picture.large} /> */}
-    </div>
-    <div className="content">
-      <ul>
-        <li>
-          <strong>Name:</strong> {this.props.name}
-        </li>
-        <li>
-          <strong>Phone:</strong> {this.props.phone}
-        </li>
-        <li>
-          <strong>Email:</strong> {this.props.email}
-        </li>
-      </ul>
-    </div>
-   
-  </div>
-          </div>
-          
-        );
-      }
-    }
-    
-    
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
+}
+
+
+
 export default App;
 
--- src/components/FriendCard/FriendCard.js
+++ src/components/FriendCard/FriendCard.js
@@ -1,26 +1,19 @@
 import React from "react";
 import "./FriendCard.css";
+import Table from 'react-bootstrap/Table'
 
 const FriendCard = props => (
-  <div className="card">
-    <div className="img-container">
-      <img alt={props.name} src={props.image} />
-    </div>
-    <div className="content">
-      <ul>
-        <li>
-          <strong>Name:</strong> {props.name}
-        </li>
-        <li>
-          <strong>Phone:</strong> {props.phone}
-        </li>
-        <li>
-          <strong>Email:</strong> {props.email}
-        </li>
-      </ul>
-    </div>
-   
-  </div>
+
+  
+    <tr>
+       <td>{props.id}</td>
+      <td>{props.name}</td>
+      <td>{props.email}</td>
+      <td>{props.phone}</td>
+      <td> <img alt={props.name} src={props.image} /> </td>
+    </tr>
+  
+
 );
 
 export default FriendCard;
